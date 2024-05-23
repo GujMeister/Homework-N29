8a3b84ead71702b564fbbf368cc36eb61dece889
GujMeister <90295019+GujMeister@users.noreply.github.com>
2024-05-23 23:58:26 +0400

    Initial Commit

---
+++ SwiftUI Essentials/SwiftUI_EssentialsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_EssentialsApp.swift
+//  SwiftUI Essentials
+//
+//  Created by Luka Gujejiani on 23.05.24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_EssentialsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
